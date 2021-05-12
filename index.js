718be47ad7a5ea020e9ae3610d493359ded3fa03
joao Rodrigo <=>
2021-05-11 23:22:18 -0300

    configurações e arquivo de teste das classes.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import Client from './src/POO/Domain/Client.js';
+// import Client from './src/POO/Domain/Client.js';
+// const cliente = new Client( 'Jose', '132575645' );
 
-const cliente = new Client('Jose', '132575645');
+import Director from './src/POO/Domain/Director.js';
+import Manager from './src/POO/Domain/Manager.js';
+import AuthenticationSystem from './src/POO/services/AuthenticationSystem.js';
 
+const manager = new Manager( 'Rodrigo', 456987545, 2895 );
+const director = new Director( 'Maria', 654852465, 5600 );
+manager.registerPassword( '12345' );
 
+console.log( manager );
+console.log( director );
 
-console.log(cliente.nome);
+const statusAuthenticate = AuthenticationSystem.login( manager, '12345' );
+console.log(  statusAuthenticate ? 'Autenticado' : 'Não autenticado!' );
